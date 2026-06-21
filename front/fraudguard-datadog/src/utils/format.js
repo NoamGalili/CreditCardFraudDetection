7ce8eeeb83bc0fe8e08b43351828aee51b95b465
Asher  Pe'er <asher.colman21@gmail.com>
2026-06-21 15:53:01 +0300

    Organize final fraud detection project

--- front/fraudguard-datadog/src/utils/format.js
+++
@@ -1,2 +0,0 @@
-export const money = n => new Intl.NumberFormat('en-US',{style:'currency',currency:'USD',maximumFractionDigits:0}).format(n);
-export const cls = (...x) => x.filter(Boolean).join(' ');
